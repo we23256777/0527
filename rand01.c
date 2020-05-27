ac775fc0bf89c61ff5c05e1b4d32141e4c4a0122
we23256777 <63162705+we23256777@users.noreply.github.com>
2020-05-27 11:01:27 +0900

    Add files via upload

---
+++ rand01.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+	int i;
+	srand(10);
+	for(i=1 i<=15; i++)
+	    printf("%2d : %5d\n",n (rand));
+	
+	return 0;	 
+	
+}
+
---
+++ rand02.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<stdilb.h>
+#include<time.h> 
+int main()
+{
+	int i;
+	srand(time(NULL));
+	for(i=1 i<=15; i++)
+	    printf("%2d : %5d\n",n (rand));
+	
+	return 0;	 
+	
+}
+
